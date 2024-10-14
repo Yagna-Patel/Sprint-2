3e7dad3d64d4f50af5b58d8ea26113f071e0c290
Yagna-Patel <123590214+Yagna-Patel@users.noreply.github.com>
2024-10-14 10:28:56 -0500

    Update sos_test.py

--- sos_test.py
+++ sos_test.py
@@ -1,14 +1,15 @@
 import pytest
 from sos_game import sos_game
 
-
-def test_letter_placement():
+# used stackoverflow to understand how assert works.
+def test_letter_placement(): # test to check that letter is placed correctly in empty cell.
     game = sos_game()
-    assert game.place_letter(0, 0, 'S') == True
-    assert game.board[0][0] == 'S'
-    assert game.current_player == 'red'
+    assert game.place_letter(0, 0, 'S') == True # place S at row 0 and col 0.
+    assert game.board[0][0] == 'S' # if S os placed correctly.
+    assert game.current_player == 'red' # switch player.
 
-def test_invalid_placement():
+# checking to see if its false when trying to place letter in occupied cell.
+def test_invalid_placement(): 
     game = sos_game()
-    game.place_letter(0, 0, 'S')
-    assert game.place_letter(0, 0, 'O') == False  # can't place a letter in the same spot
+    game.place_letter(0, 0, 'S') # place S at row 0 and col 0 to occupy the cell.
+    assert game.place_letter(0, 0, 'O') == False  # if place O in occpied cell it should be false.
